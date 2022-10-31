f0f1f2e282b5736c14f0cce65494967df95ba854
Patrik Csikós <61376206+pacsinta@users.noreply.github.com>
2022-10-31 12:36:12 +0100

    Update Skeleton.cpp

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -18,7 +18,7 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : Csikós Patrik
+// Nev    : Csikï¿½s Patrik
 // Neptun : E4MZUV
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
@@ -63,9 +63,11 @@ const int nTesselatedVertices = 100;
 
 
 class Poly {
-    std::vector<vec2> Points;
-    unsigned int vao;
-    unsigned int vbo;
+    std::vector<vec2> points;
+    std::vector<float> vertexData;
+
+    unsigned int vaoPoints, vboPoints;
+    unsigned int vaoCurve, vboCurve;
 
     int tension = -1;
     std::vector<float> ts;
@@ -73,36 +75,107 @@ public:
     vec3 color = vec3(0, 1, 1);
 
     void addPoint(vec2 new_point) {
-        Points.push_back(new_point);
+        ts.push_back(points.size());
+        points.push_back(new_point);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, Points.size() * sizeof(vec2), Points.data(), GL_STATIC_DRAW);
-
-        glutPostRedisplay();
+        calcCurve();
+        load();
+        //loadCurve();
     }
 
-    void catmullRom(){
+    vec2 Hermite(vec2 p0, vec2 v0, float t0, vec2 p1, vec2 v1, float t1, float t) {
+        vec2 a0 = p0;
+        vec2 a1 = v0;
+        vec2 a2 = (3 * (p1 - p0)) / pow(t1 - t0, 2) - (2 * v0 + v1) / (t1 - t0);
+        vec2 a3 = (2 * (p0 - p1)) / pow(t1 - t0, 3) + (v0 + v1) / pow(t1 - t0, 2);
 
+        return a3*pow(t - t0, 3) + a2*pow(t - t0, 2) + a1*(t - t0) + a0;
+    }
+
+    vec2 v(vec2 r, vec2 r0, vec2 r1, float t, float t0, float t1) {
+        float alpha = (1-tension)/2;
+        return alpha * (((r1 - r) / (t1 - t)) + ((r - r0) / (t - t0)));
+    }
+
+    vec2 catmullRom(float t){
+        for (int index = 0; index < ts.size(); ++index) {
+            int i = index + 1 == ts.size() ? 0 : index + 1;
+            if(ts[i] <= t && t >= ts[i+1]){
+                i = i - 1 < 0 ? (int)ts.size() - 1 : i - 1;
+
+                vec2 v0 = v(points[i], points[i-1], points[i+1], ts[i], ts[i-1], ts[i+1]);
+                vec2 v1 = v(points[i+1], points[i], points[i+2], ts[i+1], ts[i], ts[i+2]);
+
+                return Hermite(points[i], v0, ts[i], points[i+1], v1, ts[i+1], t);
+            }
+        }
+    }
+
+    static float tStart(){
+        return 0;
+    }
+
+    float tEnd(){
+        return (float)ts.size();
+    }
+
+    Poly(){
+        /*glGenVertexArrays(1, &vaoPoints);
+        glGenBuffers(1, &vboPoints);
+        glGenVertexArrays(1, &vaoCurve);
+        glGenBuffers(1, &vboCurve);*/
+    }
+
+    void calcCurve(){
+        if(points.size() > 2){
+            for (int i = 0; i < nTesselatedVertices; ++i) {
+                float tNormalized = (float)i / (nTesselatedVertices - 1);
+                float t = tStart() + (tEnd() - tStart()) * tNormalized;
+
+                vec2 wVertex = catmullRom(t);
+                vertexData.push_back(wVertex.x);
+                vertexData.push_back(wVertex.y);
+            }
+        }
     }
 
     void load() {
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-        glGenBuffers(1, &vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, Points.size() * sizeof(vec2), &Points[0], GL_STATIC_DRAW);
+        glGenVertexArrays(1, &vaoPoints);
+        glBindVertexArray(vaoPoints);
+        glGenBuffers(1, &vboPoints);
+        glBindBuffer(GL_ARRAY_BUFFER, vboPoints);
+        glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(vec2), points.data(), GL_STATIC_DRAW);
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vec2), reinterpret_cast<void*>(0));
-        gpuProgram.setUniform(color, "color");
+    }
+
+    void loadCurve(){
+        glGenVertexArrays(2, &vaoCurve);
+        glBindVertexArray(vaoCurve);
+        glGenBuffers(2, &vboCurve);
+        glBindBuffer(GL_ARRAY_BUFFER, vboCurve);
+        glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(float), &vertexData[0], GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, reinterpret_cast<void*>(0));
+        gpuProgram.setUniform(vec3(1, 0, 0), "color");
+    }
+
+    void drawCurve() {
+        gpuProgram.setUniform(vec3(1, 0, 0), "color");
+        glBindVertexArray(vaoCurve);
+        glBindBuffer(GL_ARRAY_BUFFER, vboCurve);
+        glDrawArrays(GL_LINE_STRIP, 0, nTesselatedVertices);
     }
 
     void draw() {
-        glBindVertexArray(vao);
-        glDrawArrays(GL_LINE_LOOP, 0, Points.size());
+        gpuProgram.setUniform(color, "color");
+        glBindVertexArray(vaoPoints);
+        glBindBuffer(GL_ARRAY_BUFFER, vboPoints);
+        glDrawArrays(GL_LINE_LOOP, 0, points.size());
     }
 };
 
-Poly polygon;
+Poly *polygon;
 
 // Initialization, create an OpenGL context
 void onInitialization() {
@@ -111,8 +184,7 @@ void onInitialization() {
     glLineWidth(2);
 
     gpuProgram.create(vertexSource, fragmentSource, "outColor");
-    polygon = Poly();
-    polygon.load();
+    polygon = new Poly();
 }
 
 // Window has become invalid: Redraw
@@ -128,6 +200,14 @@ void onDisplay() {
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
     if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+
+    if(key == 's'){
+        polygon.calcCurve();
+        polygon.loadCurve();
+        polygon.drawCurve();
+
+        glutPostRedisplay();
+    }
 }
 
 // Key of ASCII code released
@@ -154,6 +234,8 @@ void onMouse(int button, int state, int pX,
 
         printf("Mouse click at coordinates (%.5f, %.5f)\n", cX, cY);
         polygon.addPoint(vec2(cX, cY));
+
+        glutPostRedisplay();
     }
 }
 
